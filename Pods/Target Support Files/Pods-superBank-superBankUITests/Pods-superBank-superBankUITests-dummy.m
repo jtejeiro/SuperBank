3a3e47652930dbbdeae1fce5d55972406f875304
jaimetejeiro <jaimetejeiro@gmail.com>
2020-05-12 14:07:13 +0200

    remove carpeta pod

--- Pods/Target Support Files/Pods-superBank-superBankUITests/Pods-superBank-superBankUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_superBank_superBankUITests : NSObject
-@end
-@implementation PodsDummy_Pods_superBank_superBankUITests
-@end
--- Pods/Target Support Files/Pods-superBankTests/Pods-superBankTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_superBankTests : NSObject
-@end
-@implementation PodsDummy_Pods_superBankTests
-@end
